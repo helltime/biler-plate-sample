0bf03e352c00ba71d31a4091df661f0966ce4ddb
DESKTOP-T0GQC0L\Administrator <helltime@naver.com>
2021-01-01 15:25:41 +0900

    test2

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser')
+const config = require('./config/key')
 
+const { User } = require('./models/User')
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+//application/json
 const mongoose = require('mongoose')
 
-console.log('test');
-
-mongoose.connect('mongodb+srv://mongodb_user:vyhfACWSDj1gTOuE@cluster0.bpwbn.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
@@ -17,9 +24,28 @@ mongoose.connect('mongodb+srv://mongodb_user:vyhfACWSDj1gTOuE@cluster0.bpwbn.mon
 console.log('test2');
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!!!!!!!!!!!!!!!')
+})
+
+app.post('/register', (req, res) => {
+    //회원가입 정보
+
+    const user = new User(req.body)
+
+
+    user.save((err, userInfo) => {
+        if(err) {
+            return res.json({ 
+                success: false, err
+            })
+        }
+
+        return res.status(200).json({
+            success: true
+        })
+    })
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
