d4cf5bd90c3d217fcea4a963dfdf9e3566968702
helltime <76647666+helltime@users.noreply.github.com>
2021-01-28 11:22:15 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,30 +1,34 @@
 <script>
 	var arrObj = [{"_id": 1, "parentId": null, "comment": "abcd1", "contentId": "555555"}
-	            , {"_id": 1, "parentId": null, "comment": "abcd1", "contentId": "555555"}
-				, {"_id": 1, "parentId": null, "comment": "abcd1", "contentId": "555555"}
-				, {"_id": 1, "parentId": null, "comment": "abcd1", "contentId": "555555"}
-				, {"_id": 1, "parentId": null, "comment": "abcd1", "contentId": "555555"}
-				, {"_id": 1, "parentId": null, "comment": "abcd1", "contentId": "555555"}
-	            , {"_id": 1, "parentId": null, "comment": "abcd1", "contentId": "555555"}
-				, {"_id": 1, "parentId": null, "comment": "abcd1", "contentId": "555555"}
-				, {"_id": 1, "parentId": null, "comment": "abcd1", "contentId": "555555"}
-				, {"_id": 1, "parentId": null, "comment": "abcd1", "contentId": "555555"}
-				, {"_id": 1, "parentId": null, "comment": "abcd1", "contentId": "555555"}
-	            , {"_id": 1, "parentId": null, "comment": "abcd1", "contentId": "555555"}
-				, {"_id": 1, "parentId": null, "comment": "abcd1", "contentId": "555555"}
-				, {"_id": 1, "parentId": null, "comment": "abcd1", "contentId": "555555"}
-				, {"_id": 1, "parentId": null, "comment": "abcd1", "contentId": "555555"}
-				, {"_id": 1, "parentId": null, "comment": "abcd1", "contentId": "555555"}
-	            , {"_id": 1, "parentId": null, "comment": "abcd1", "contentId": "555555"}
-				, {"_id": 1, "parentId": null, "comment": "abcd1", "contentId": "555555"}
-				, {"_id": 1, "parentId": null, "comment": "abcd1", "contentId": "555555"}
-				, {"_id": 1, "parentId": null, "comment": "abcd1", "contentId": "555555"}
-				, {"_id": 1, "parentId": null, "comment": "abcd1", "contentId": "555555"}
-				, {"_id": 1, "parentId": null, "comment": "abcd1", "contentId": "555555"}
-				, {"_id": 1, "parentId": null, "comment": "abcd1", "contentId": "555555"}
-				, {"_id": 1, "parentId": null, "comment": "abcd1", "contentId": "555555"}
-				, {"_id": 1, "parentId": null, "comment": "abcd1", "contentId": "555555"}
-				, {"_id": 1, "parentId": null, "comment": "abcd1", "contentId": "555555"}
+	            , {"_id": 2, "parentId": 8, "comment": "abcd1", "contentId": "555555"}
+				, {"_id": 3, "parentId": 8, "comment": "abcd1", "contentId": "555555"}
+				, {"_id": 4, "parentId": 7, "comment": "abcd1", "contentId": "555555"}
+				, {"_id": 5, "parentId": 7, "comment": "abcd1", "contentId": "555555"}
+				, {"_id": 6, "parentId": 1, "comment": "abcd1", "contentId": "555555"}
+	            , {"_id": 7, "parentId": 1, "comment": "abcd1", "contentId": "555555"}
+				, {"_id": 8, "parentId": 1, "comment": "abcd1", "contentId": "555555"}
+				, {"_id": 9, "parentId": null, "comment": "abcd1", "contentId": "555555"}
+				, {"_id": 10, "parentId": 9, "comment": "abcd1", "contentId": "555555"}
+				, {"_id": 11, "parentId": 9, "comment": "abcd1", "contentId": "555555"}
+	            , {"_id": 12, "parentId": 11, "comment": "abcd1", "contentId": "555555"}
+				, {"_id": 13, "parentId": 9, "comment": "abcd1", "contentId": "555555"}
+				, {"_id": 14, "parentId": 13, "comment": "abcd1", "contentId": "555555"}
+				, {"_id": 15, "parentId": 13, "comment": "abcd1", "contentId": "555555"}
+				, {"_id": 16, "parentId": null, "comment": "abcd1", "contentId": "555555"}
+	            , {"_id": 17, "parentId": 11, "comment": "abcd1", "contentId": "555555"}
+				, {"_id": 18, "parentId": 8, "comment": "abcd1", "contentId": "555555"}
+				, {"_id": 19, "parentId": 7, "comment": "abcd1", "contentId": "555555"}
+				, {"_id": 20, "parentId": 6, "comment": "abcd1", "contentId": "555555"}
+				, {"_id": 21, "parentId": 16, "comment": "abcd1", "contentId": "555555"}
+				, {"_id": 22, "parentId": 16, "comment": "abcd1", "contentId": "555555"}
+				, {"_id": 23, "parentId": 22, "comment": "abcd1", "contentId": "555555"}
+				, {"_id": 24, "parentId": 22, "comment": "abcd1", "contentId": "555555"}
+				, {"_id": 25, "parentId": 16, "comment": "abcd1", "contentId": "555555"}
+				, {"_id": 26, "parentId": 25, "comment": "abcd1", "contentId": "555555"}
+				, {"_id": 27, "parentId": 25, "comment": "abcd1", "contentId": "555555"}
+				, {"_id": 28, "parentId": 18, "comment": "abcd1", "contentId": "555555"}
+				, {"_id": 29, "parentId": 18, "comment": "abcd1", "contentId": "555555"}
+				, {"_id": 30, "parentId": 18, "comment": "abcd1", "contentId": "555555"}		
 	];
 	
 	let fn_tree = function(arrObj) {
